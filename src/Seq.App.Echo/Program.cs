41d8745268bcbd9dfaaade3d893d9ac5180a6b67
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2018-06-21 19:06:21 +1000

    Initial version

---
+++ src/Seq.App.Echo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Seq.App.Echo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Console.InputEncoding = new UTF8Encoding(false);
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                Console.Error.WriteLine(line);
+            }
+        }
+    }
+}
